7bb718b5886c2f708311118cce8ed13d526b709f
Aniket Shende <aniketshende1309@gmail.com>
2019-02-10 20:36:40 +0530

    checking the Jenkins integration

--- Jenkinstwo.java
+++ Jenkinstwo.java
@@ -5,6 +5,8 @@ public class Jenkinstwo {
 	public static void main(String[] args) {
 		
 		System.out.println("This is second file");
+		
+		System.out.println("Committing the second file");
 
 	}
 
